f320d33f93a2a60ea237b31e11a4d6303ae2e52f
Sergey <sergey.dvornikov@gmail.com>
2014-10-09 21:37:39 -0400

    added AgentRepairCode to clear database action

--- TerminalTracker/Classes/STTTMainTVC.m
+++ TerminalTracker/Classes/STTTMainTVC.m
@@ -14,6 +14,7 @@
 #import "STTTInfoCell.h"
 #import "STTTSyncer.h"
 #import <STManagedTracker/STLogMessage+dayAsString.h>
+#import "STTTAgentRepairCode.h"
 
 @interface STTTMainTVC () <UIAlertViewDelegate>
 
@@ -475,6 +476,7 @@
     
     [self removeObjectWithName:NSStringFromClass([STTTAgentTask class])];
     [self removeObjectWithName:NSStringFromClass([STTTAgentTerminal class])];
+    [self removeObjectWithName:NSStringFromClass([STTTAgentRepairCode class])];
     [self removeObjectWithName:NSStringFromClass([STLogMessage class])];
 
     [(STTTSyncer *)self.session.syncer setDataOffset:nil];
